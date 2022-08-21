7ae0210ce1447f35e51faead72a3d00ad7f5c1a7
YASH THAKAR <89722428+YT-95@users.noreply.github.com>
2022-08-21 23:20:45 +0530

    Test 1 passed

---
+++ calculator/StringCalculator.java
@@ -0,0 +1,6 @@
+package calculator;
+//Test cases
+//1. Add
+public class StringCalculator {
+
+}
---
+++ calculator/TestClassForStringCalculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestClassForStringCalculator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
